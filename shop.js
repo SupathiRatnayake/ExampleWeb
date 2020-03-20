b8aead6bb903b335dfe388d7f0862d88a2b671a8
SupathiRatnayake <r.supathi@gmail.com>
2020-03-21 00:13:48 +0530

    3/21/2020 12.13am

--- shop.js
+++ shop.js
@@ -22,7 +22,7 @@ function Leaf(id, name, img, descrip, price) {
   this.price = price;
 
   this.makeElement = () =>{
-    let item, img, dl, dtName, ddName, dtPrice, ddPrice, label, input, divBar;
+    let item, img, dl, dtName, ddName, dtPrice, ddPrice;
     item = document.createElement("div");
     item.setAttribute("class", "item");
     img = this.img.makeElement();
@@ -38,25 +38,11 @@ function Leaf(id, name, img, descrip, price) {
     dtPrice.innerHTML = "Price";
     ddPrice = document.createElement("dd");
     ddPrice.setAttribute("class", "currency")
-    ddPrice.innerHTML = this.price;
+    ddPrice.innerHTML = parseFloat(this.price).toFixed(2);
 
     dl.append(dtName, ddName, dtPrice, ddPrice);
 
-    label = document.createElement("label");
-    label.setAttribute("for", this.id);
-    label.innerHTML = "Add to Cart";
-
-    input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("id", this.id);
-    input.setAttribute("value", this.name);
-    input.setAttribute("onclick", "selectItem(this)");
-
-    divBar = document.createElement("div");
-    divBar.setAttribute("class", "addToCart");
-    divBar.append(label, input);
-
-    item.append(img, dl, divBar);
+    item.append(img, dl);
 
     return item;
   }
@@ -64,13 +50,13 @@ function Leaf(id, name, img, descrip, price) {
 
 let img1 = new Img("green_leaf.jpg", "green_leaf");
 
-let l1 = new Leaf("l1", "Leaf 1", new Img("green_leaf.jpg", "green_leaf"), "abc", 5.00);
-let l2 = new Leaf("l2", "Leaf 2", new Img("pink_leaf.jpg", "pink_leaf"), "def", 10.00);
-let l3 = new Leaf("l3", "Leaf 3", new Img("brown_leaf.jpg", "pink_leaf"), "ghi", 15.50);
-let l4 = new Leaf("l4", "Leaf 4", new Img("yellow_leaf.jpg", "pink_leaf"), "jkl", 10.00);
-let l5 = new Leaf("l5", "Leaf 5", new Img("green_leaf.jpg", "pink_leaf"), "mno", 20.00);
-let l6 = new Leaf("l6", "Leaf 6", new Img("pink_leaf.jpg", "pink_leaf"), "pqr", 7.00);
-let l7 = new Leaf("l7", "Leaf 7", new Img("yellow_leaf.jpg", "pink_leaf"), "stu", 10.50);
+let l1 = new Leaf("l1", "Shizpierce", new Img("green_leaf.jpg", "green_leaf"), "Aenean commodo ac purus id sollicitudin.", 5.00);
+let l2 = new Leaf("l2", "Alexanderend", new Img("pink_leaf.jpg", "pink_leaf"), "Nulla pulvinar massa quis magna laoreet blandit.", 10.00);
+let l3 = new Leaf("l3", "Rodriguedrin ", new Img("brown_leaf.jpg", "pink_leaf"), "Curabitur nec purus sed ipsum molestie.", 15.50);
+let l4 = new Leaf("l4", "Tuxie", new Img("yellow_leaf.jpg", "pink_leaf"), "Proin suscipit aliquet pharetra.", 10.00);
+let l5 = new Leaf("l5", "Whiteya", new Img("green_leaf.jpg", "pink_leaf"), "Praesent quis pellentesque elit.", 20.00);
+let l6 = new Leaf("l6", "Forestson", new Img("pink_leaf.jpg", "pink_leaf"), "Morbi finibus facilisis sapien.", 7.00);
+let l7 = new Leaf("l7", "Glenkelley", new Img("yellow_leaf.jpg", "pink_leaf"), "Integer et ligula at urna varius ornare.", 10.50);
 
 let leaves = [l1, l2, l3, l4, l5, l6, l7];
 
@@ -96,6 +82,24 @@ let selectItem = (e) => {
 
 leaves.forEach((item, i) => {
   itemBox.appendChild(item.makeElement());
+
+  let label, input, divBar;
+
+  label = document.createElement("label");
+  label.setAttribute("for", item.id);
+  label.innerHTML = "Add to Cart";
+
+  input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("id", item.id);
+  input.setAttribute("value", item.name);
+  input.setAttribute("onclick", "selectItem(this)");
+
+  divBar = document.createElement("div");
+  divBar.setAttribute("class", "addToCart");
+  divBar.append(label, input);
+
+  itemBox.appendChild(divBar);
 });
 
 let selectedItems = [];
@@ -112,12 +116,14 @@ function purchase(form) {
   let inputList = Array.prototype.slice.call(inputs);
   console.log(inputs);
 
-  inputList.forEach((item, i) => {
-    if (item.value.length == 0) {
-      alert("Enter data fucker!\n" + "You've left " + item.name + " empty!");
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == "") {
+      alert("Please fill " + inputs[i].name + " or I'll call the police!");
+      inputs[i].focus();
+      return false;
     }
-    console.log(item.value);
-  });
+
+  }
 
   let msg = "";
   selectedItems.forEach((item, i) => {
